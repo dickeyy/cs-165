2ef36db2d8724cc948f1672cf748368e7e5af628
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-02-23 12:30:54 -0700

    ws-13 program-5

---
+++ worksheet/ws-13/Worksheet.java
@@ -0,0 +1,19 @@
+public class Worksheet {
+    /*
+     * 1. The contents of the ArrayList will be:
+     * Stack, ArrayList, Queue,
+     * 
+     * 2. True
+     * 
+     * 3a. the arraylist above doesnt contain any ints becuase the arraylist is
+     * of type Integer and not int
+     * 
+     * 3b. [0,1,2,3,4] -> [0,2,4]
+     * 
+     * 4. 4.0
+     * 3.0
+     * 
+     * 5. 4 5.0
+     * a true
+     */
+}
