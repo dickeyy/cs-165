11760152c18c8d7f79821f7a6c0f7e4d38d94250
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-03-05 13:21:13 -0700

    ws-14

---
+++ worksheet/ws-15/Worksheet.java
@@ -0,0 +1,17 @@
+public class Worksheet {
+    /*
+     * 1. b
+     * 2. c
+     * 3. a
+     * 4. a
+     * 5. 3
+     * 6. a
+     * 7. b
+     * 8. a
+     * 9. 90, 22
+     * 10. Creates a new stack then pushes every character of a given string to the
+     * stack, then pops every character from the stack and appends it to a new
+     * string. The new string is then returned.
+     * 11. 6
+     */
+}
