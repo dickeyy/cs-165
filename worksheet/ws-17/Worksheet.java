e0bc8eb02f6262f79c69d4158a404dd94406d8c1
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-03-22 12:27:30 -0600

    ws-17

--- worksheet/ws-17/Worksheet.java
+++ worksheet/ws-17/Worksheet.java
@@ -1,9 +1,14 @@
 public class Worksheet {
-    public E element() {
-        if (last == null) {
-            throw new NoSuchElementException();
-        }
-        NodeGeneric<E> head = last.next;
-        return head.getElement();
-    }
+    /*
+     * 1. Stack, Queue
+     * 2. Queue
+     * 3. No
+     * 4. 5 2 4 0
+     * 5.
+     * 6.
+     * 7. S1 : 2 (top)
+     * S2 : 1 2 2 3 3 (top)
+     * Q : (head) 3 3
+     * 8.
+     */
 }
